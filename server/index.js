7f7ab21320826c6b795f3aa5b2b405c34f032a83
Dilaxn <dilaxn@gmail.com>
2021-11-27 09:39:22 +0530

    suggestion sample added

--- server/index.js
+++ server/index.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const bodyParser = require('body-parser')
+const cors = require('cors');
 
 const PORT = process.env.PORT || 3001;
 
@@ -6,9 +8,19 @@ const app = express();
 require('dotenv').config();
 require('./db/mongoose');
 
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
+
 app.get("/api", (req, res) => {
     res.send({ message: "Hello from server!" });
 });
+
+
+
+const suggestion_router = require('./src/routes/suggestion');
+
+app.use(cors())
+app.use(suggestion_router);
 
 app.listen(PORT, () => {
     console.log(`Server listening on ${PORT}`);
